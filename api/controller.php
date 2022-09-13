8b923876dcafb909d479c4a93265bfde831bde5c
renanmetzenif <renanmetzenif@gmail.com>
2022-09-13 17:48:27 -0300

    pronto cesar

--- api/controller.php
+++ api/controller.php
@@ -1,10 +1,23 @@
 <?php
     if($_POST["radio"] == "cesar") {
+        $arrayNovasPalavras = array();
         $formatosPermitidos = "txt";
         $extensao = pathinfo($_FILES['arquivo']['name'], PATHINFO_EXTENSION);
-        if(1){
+        if($extensao == $formatosPermitidos){
             $temporario = $_FILES['arquivo']['tmp_name'];
             $palavra = file_get_contents($temporario);
+            $arrayPalavra = str_split($palavra);
+            $arrayPalavras = explode("\n", file_get_contents("./resources/palavras.txt"));
+            $arrayLetras = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+            for ($i=1; $i < count($arrayLetras); $i++) {
+                $palavraPronta = '';
+                for ($j=0; $j < count($arrayPalavra); $j++) {
+                    $letra = $arrayLetras[(array_search($arrayPalavra[$j], $arrayLetras) + $i) % 26];
+                    $palavraPronta .= $letra;
+                }
+                $arrayNovasPalavras[] = $palavraPronta;
+            }
         }
-        echo json_encode($palavra);
+        echo json_encode($arrayNovasPalavras);
     }
+?>
