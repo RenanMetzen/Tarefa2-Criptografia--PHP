8664b3a8b53e3adaf048c8193164639e2204788c
renanmetzenif <renanmetzenif@gmail.com>
2022-09-13 17:52:47 -0300

    pronto cesar

--- api/controller.php
+++ api/controller.php
@@ -7,7 +7,7 @@
             $temporario = $_FILES['arquivo']['tmp_name'];
             $palavra = file_get_contents($temporario);
             $arrayPalavra = str_split($palavra);
-            $arrayPalavras = explode("\n", file_get_contents("./resources/palavras.txt"));
+            $arrayPalavras = explode("\n", file_get_contents("resources/palavras.txt"));
             $arrayLetras = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
             for ($i=1; $i < count($arrayLetras); $i++) {
                 $palavraPronta = '';
